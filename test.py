0e3800f8ebdc93edbb548314cb67eb5a685546c5
ruth <131242482+ruth8607253@users.noreply.github.com>
2023-10-05 17:31:56 +0800

    上課筆記

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main():
+    a=["aaa","bbb","ccc"]
+    return render_template('main.jinja.html',a=a)
+
+@app.route("/feature")
+def feature():
+    return render_template('feature.jinja.html')
+
+@app.route("/child")
+def child():
+    return render_template('child.jinja.html')
+
+@app.route("/book")
+def book():
+    return render_template('book.jinja.html')
